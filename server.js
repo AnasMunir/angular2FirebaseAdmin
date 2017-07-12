e712c0720e9e15aeaee1efd0184cb123f12f1245
anas munir <anas@anass-MacBook-Pro.local>
2017-07-13 03:33:56 +0500

    fixing possible router conflict 24c4aaaadf287

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const bodyParser = require('body-parser');
+const api = require('./server/routes/api');
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Run the app by serving the static files
 // in the dist directory
-// app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+app.use('/api', api)
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
-});
+function redirectRouter(req, res) {
+  res.sendFile("index.html", { root: './dist'})
+}
+app.use(redirectRouter);
+
+// app.get('/*', (req, res) => {
+//     res.sendFile("index.html", { root: './dist'})
+// });
+app.listen(process.env.PORT || 8080);
