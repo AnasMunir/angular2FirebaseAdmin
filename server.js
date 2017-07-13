7a50a3079cec38057f92d3633d6c4f5da7f6962d
anas munir <anas@anass-MacBook-Pro.local>
2017-07-13 10:43:25 +0500

    going back to initial stage

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const bodyParser = require('body-parser');
-const api = require('./server/routes/api');
+// const bodyParser = require('body-parser');
+// const api = require('./server/routes/api');
 
 // Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.use('/api', api)
+// app.use('/api', api)
 
 function redirectRouter(req, res) {
   res.sendFile("index.html", { root: './dist'})
