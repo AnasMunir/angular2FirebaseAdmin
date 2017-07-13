9bcb1c67fd0e85020ba0fdc510804c8501b5fa03
anas munir <anas@anass-MacBook-Pro.local>
2017-07-13 11:36:13 +0500

    trying to apis work

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ admin.initializeApp({
     databaseURL: "https://onelegacy-f0695.firebaseio.com/"
 })
 
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
 
 const db = admin.database();
 var ref = db.ref('users/');
