edf6788089439e241f3931d36a4bfc7c14f3b53c
anas munir <anas@anass-MacBook-Pro.local>
2017-07-17 17:24:16 +0500

    logging results

---
+++ src/app/customer-detail/customer-detail.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CustomerService } from "../_services/customer.service";
+import { ParamMap, ActivatedRoute } from "@angular/router";
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+@Component({
+  selector: 'app-customer-detail',
+  templateUrl: './customer-detail.component.html',
+  styleUrls: ['./customer-detail.component.css']
+})
+export class CustomerDetailComponent implements OnInit {
+
+  customerDetail: any;
+
+  constructor(private cs: CustomerService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.getCustomerDetails();
+  }
+
+  private async getCustomerDetails() {
+    let id = this.route.snapshot.params['id'];
+    console.log("id", id);
+    this.customerDetail = await this.cs.getCustomerDetails(id).take(1).toPromise();
+    console.log(this.customerDetail);
+    
+  }
+
+}
