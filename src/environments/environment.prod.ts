ca8c0d23424a7d665320f81405e3777a8fd89b30
anas munir <anas@anass-MacBook-Pro.local>
2017-07-17 13:12:38 +0500

    error in environment resolving

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDyK596cuHRTzGK_CHQIYHk4hIQJFb7W1k",
+    authDomain: "onelegacy-f0695.firebaseapp.com",
+    databaseURL: "https://onelegacy-f0695.firebaseio.com",
+    projectId: "onelegacy-f0695",
+    storageBucket: "onelegacy-f0695.appspot.com",
+    messagingSenderId: "107640986523"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
     apiKey: "AIzaSyDyK596cuHRTzGK_CHQIYHk4hIQJFb7W1k",
     authDomain: "onelegacy-f0695.firebaseapp.com",
